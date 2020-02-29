34158a185c83f6cd661ff94e60809d175ffd0a70
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-02-29 14:41:21 +0600

    initializing

---
+++ src/finddigits/FindDigits.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finddigits;
+
+/**
+ *
+ * @author Foysal
+ */
+public class FindDigits {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
