1b5912f1401c08500b70d1b73ee179226bb6748f
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-02-29 15:20:55 +0600

    Complete

--- src/finddigits/FindDigits.java
+++ src/finddigits/FindDigits.java
@@ -4,6 +4,14 @@
  * and open the template in the editor.
  */
 package finddigits;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.*;
 
 /**
  *
@@ -14,8 +22,42 @@ public class FindDigits {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+
+        int count=0;
+        int numberOfDigits = (""+n).length();
+        for(int i=0; i<numberOfDigits; i++){
+           int digit=Character.getNumericValue((""+n).charAt(i));
+           if( (digit!=0) && (n%digit==0) )count++;
+        }
+        return count;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
     
 }
