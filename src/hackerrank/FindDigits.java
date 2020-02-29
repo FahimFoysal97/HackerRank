07df23ecdadb5a581b1cd85d9a88b4805e6a4c87
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-02-29 15:22:16 +0600

    Complete

---
+++ src/hackerrank/FindDigits.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+/**
+ *
+ * @author Foysal
+ */
+public class FindDigits {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+
+        int count=0;
+        int numberOfDigits = (""+n).length();
+        for(int i=0; i<numberOfDigits; i++){
+           int digit=Character.getNumericValue((""+n).charAt(i));
+           if( (digit!=0) && (n%digit==0) )count++;
+        }
+        return count;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+    
+}
