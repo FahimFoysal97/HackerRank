b0d9f29a9ba204861358946ee07f7670a1a9ad57
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-01 15:43:11 +0600

    Complete

---
+++ src/hackerrank/EqualizeTheArray.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ *
+ * @author Foysal
+ */
+public class EqualizeTheArray {
+    
+    // Complete the equalizeArray function below.
+    static int equalizeArray(int[] arr) {
+        
+        int count[] = new int[100];
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i] - 1]++; 
+            max = Math.max(max, count[arr[i]-1]);
+        }
+        
+        return arr.length - max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
