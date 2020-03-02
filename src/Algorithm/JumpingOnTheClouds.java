798416f57e9f9f4273dbb61f4a182774a15622b2
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-02 20:35:44 +0600

    Complete

---
+++ src/Algorithm/JumpingOnTheClouds.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+/**
+ *
+ * @author Foysal
+ */
+public class JumpingOnTheClouds {
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c) {
+        int i=0;
+        int steps = 0;
+        while(true){
+            if(i+2<c.length){
+                if(c[i+2]!=1){
+                    i+=2;
+                    steps++;
+                }else{
+                    i++;
+                    steps++;
+                }
+            }else if(i+1<c.length){
+                i++;
+                steps++;
+            }else break;
+        }
+        return steps;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
