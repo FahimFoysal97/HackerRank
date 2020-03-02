da408a4440a3ad14e709be262d9235a5f165b739
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-02 10:47:50 +0600

    Complete

---
+++ src/ServiceLane.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ *
+ * @author Foysal
+ */
+public class ServiceLane {
+    // Complete the serviceLane function below.
+    static int[] serviceLane(int n, int[][] cases) {
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nt = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nt[0]);
+
+        int t = Integer.parseInt(nt[1]);
+
+        int[] width = new int[n];
+
+        String[] widthItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int widthItem = Integer.parseInt(widthItems[i]);
+            width[i] = widthItem;
+        }
+
+        int[][] cases = new int[t][2];
+
+        for (int i = 0; i < t; i++) {
+            String[] casesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 2; j++) {
+                int casesItem = Integer.parseInt(casesRowItems[j]);
+                cases[i][j] = casesItem;
+            }
+        }
+
+        int[] result = serviceLane(n, cases);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+}
