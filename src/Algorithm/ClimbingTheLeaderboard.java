009c940bd337e8362c2f509821946f8295a93514
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-02 20:48:20 +0600

    Complete

--- src/Algorithm/ClimbingTheLeaderboard.java
+++ src/Algorithm/ClimbingTheLeaderboard.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Algorithm;
 import java.io.*;
 import java.math.*;
 import java.security.*;
