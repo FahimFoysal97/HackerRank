3842605669492b0783f62b0e879284d6665445a7
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-02 20:47:35 +0600

    Complete

--- src/Algorithm/ServiceLane.java
+++ src/Algorithm/ServiceLane.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hackerrank;
+package Algorithm;
 import java.io.*;
 import java.math.*;
 import java.security.*;
