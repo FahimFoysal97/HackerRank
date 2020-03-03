c39bef8d0908ba57b715f9e67ee65c8319d64c4b
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-03 14:23:23 +0600

    Complete

---
+++ src/Algorithm/SherlockAndSquares.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Algorithm;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ *
+ * @author Foysal
+ */
+
+
+public class SherlockAndSquares {
+
+    // Complete the squares function below.
+    static int squares(int a, int b) {
+        int a1 = (int)Math.sqrt(a);
+        int b1 = (int)Math.sqrt(b);
+        int n = Math.abs(a1-b1);
+        if(a1*a1==a)n++;
+        return n;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String[] ab = scanner.nextLine().split(" ");
+
+            int a = Integer.parseInt(ab[0]);
+
+            int b = Integer.parseInt(ab[1]);
+
+            int result = squares(a, b);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
