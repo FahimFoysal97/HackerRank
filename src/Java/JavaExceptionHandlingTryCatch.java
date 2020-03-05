bf201b4c6813185847be26ea270b325a85686339
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-05 17:01:06 +0600

    Complete

---
+++ src/Java/JavaExceptionHandlingTryCatch.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Foysal
+ */
+public class JavaExceptionHandlingTryCatch {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        try {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            System.out.println(x/y);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch (ArithmeticException e) {
+            System.out.println(e.getClass().getName() + ": / by zero");
+        }
+    }
+}
