9279bd9b2f9bf9c1923e2981ca8e233a08383d5f
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-05 16:32:23 +0600

    Complete

---
+++ src/Java/WelcomeToJava.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java;
+
+/**
+ *
+ * @author Foysal
+ */
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
