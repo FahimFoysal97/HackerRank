99ef9c0f92137a9f30c3fdd1e3d7fafffeeb1bd5
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-06 16:09:31 +0600

    Complete

--- src/Java/JavaDatatypes.java
+++ src/Java/JavaDatatypes.java
@@ -11,19 +11,12 @@ import java.io.*;
  * @author Foysal
  */
 public class JavaDatatypes {
-    public static void main(String []argh)
-    {
-
-
-
+    public static void main(String []argh){
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
 
-        for(int i=0;i<t;i++)
-        {
-
-            try
-            {
+        for(int i=0;i<t;i++){
+            try{
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)System.out.println("* byte");
@@ -32,8 +25,7 @@ public class JavaDatatypes {
                 if(x>=-9223372036854775808l && x<=9223372036854775807l)System.out.println("* long");
                 //Complete the code
             }
-            catch(Exception e)
-            {
+            catch(Exception e){
                 System.out.println(sc.next()+" can't be fitted anywhere.");
             }
 
