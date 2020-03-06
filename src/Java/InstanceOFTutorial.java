20401fc5057048f29e127eb50ad60e0566c18505
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-06 16:15:44 +0600

    Complete

---
+++ src/Java/InstanceOFTutorial.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java;
+import java.util.*;
+
+/**
+ *
+ * @author Foysal
+ */
+
+class Student1{}
+class Rockstar{   }
+class Hacker{}
+
+
+public class InstanceOFTutorial{
+	
+    static String count(ArrayList mylist){
+        int a = 0,b = 0,c = 0;
+        for(int i = 0; i < mylist.size(); i++){
+            Object element=mylist.get(i);
+            if(element instanceof Student1)
+                a++;
+            if(element instanceof Rockstar)
+                b++;
+            if(element instanceof Hacker)
+                c++;
+        }
+        String ret = Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
+        return ret;
+    }
+
+   public static void main(String []args){
+      ArrayList mylist = new ArrayList();
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+      for(int i=0; i<t; i++){
+         String s=sc.next();
+         if(s.equals("Student"))mylist.add(new Student1());
+         if(s.equals("Rockstar"))mylist.add(new Rockstar());
+         if(s.equals("Hacker"))mylist.add(new Hacker());
+      }
+      System.out.println(count(mylist));
+   }
+}
