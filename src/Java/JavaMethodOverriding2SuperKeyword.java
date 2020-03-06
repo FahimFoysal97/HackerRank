fe02246c4a4d460b3033ad318e3e2fb983548fc1
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-06 07:17:09 +0600

    Complete

---
+++ src/Java/JavaMethodOverriding2SuperKeyword.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java;
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author Foysal
+ */
+
+
+
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+		String temp=super.define_me(); //Fix this line
+
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+class JavaMethodOverriding2SuperKeyword{
+	public static void main(String []args){
+		MotorCycle M=new MotorCycle();
+	}
+}
