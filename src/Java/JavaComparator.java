34d81c4ce546c3bfedf6947b6d6fe0caf137a95b
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-06 15:54:10 +0600

    Complete

--- src/Java/JavaComparator.java
+++ src/Java/JavaComparator.java
@@ -14,6 +14,7 @@ import java.util.*;
 
 // Write your Checker class here
 class Checker implements Comparator {
+    @Override
     public int compare(Object o1, Object o2) {
         Player p1 = (Player) o1;
         Player p2 = (Player) o2;
