7e04365a677d817266a3d366b5cb17dc3425ae4b
Muhammad Fahim Foysal <fahimfoysal97@gmail.com>
2020-03-09 13:50:12 +0600

    Complete

---
+++ src/Algorithm_DataStructure/SolveMeFirst.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Algorithm_DataStructure;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ *
+ * @author Foysal
+ */
+
+public class SolveMeFirst {
+
+
+    static int solveMeFirst(int a, int b) {
+      	// Hint: Type return a+b; below 
+        return a+b;
+   }
+
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+   }
+}
+
